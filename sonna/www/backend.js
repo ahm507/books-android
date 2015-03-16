4a66e67b3e8300cf686859e27d1a10d8cb8bc2e2
Ahmed Hammad <ahm507@gmail.com>
2015-03-16 22:53:47 +0200

    Cleanup of TOC Search implementation

--- sonna/www/backend.js
+++ sonna/www/backend.js
@@ -1,20 +1,9 @@
 // BACK END SERVICES ONLY, NO UI HERE
 
-//var PageService = function () {
-
-//FIXME onDeviceReady issue
-
+var database;
 function initializeDB () {
     var deferred = $.Deferred();
-//      See https://github.com/brodysoft/Cordova-SQLitePlugin
-//FIXME make a programatic switch between browser mode and emulator/device mode
-    //sqlitePlugin runs only in Emulator or real device
 
-//if (screen.width <= 699) {
-//    document.location = "/mobile/index.html";
-//} else {
-//    document.location = "/desktop/index.html";
-//}
     //Show Hour glass
     $.mobile.loading( "show", {
       text: "",
@@ -23,192 +12,89 @@ function initializeDB () {
       html: ""
     });
 
-    console.log(">navigator= " + navigator.userAgent);
-
-    //>navigator= Mozilla/5.0 (Linux; Android 5.0.1; Android SDK built for x86 Build/LSX66B) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36
-
-
-    //>navigator= Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53
-
-    //it is important to remove any old pending file
     //FIXME only copy on installation, first time, not with each run
+    //it is important to remove any old pending file
 
-    //Mobile Device
     if(typeof mac_browser_test === 'undefined'){
         console.log("mac_browser_test is undefined ");
-    }
-    else {
-        console.log("mac_browser_test = ", mac_browser_test);
-    }
-
-    if(typeof mac_browser_test === 'undefined'){
         window.plugins.sqlDB.remove("sonna.sqlite", removeSuccess, removeError);
         window.plugins.sqlDB.copy("sonna.sqlite", copySuccess, copyError);
-    } else {
-        OpenMyDatabase();
+    } else { //Browser debug
+        console.log("mac_browser_test = ", mac_browser_test);
+        OpenMyDatabase(); //oncopy event will not work
+        doDisplay("g2b1", 0);
     }
-
-//        window.plugins.sqlDB.copy("sonna.sqlite", copySuccess, copyError);
-//        db = window.openDatabase("sqlite", "1.0", "sqlite", 20000000);
-//        db.transaction(
-//            function (tx) {
-//                createTable(tx);
-//                addSampleData(tx);
-//            },
-//            function (error) {
-//                console.log('Transaction error: ' + error);
-//                deferred.reject('Transaction error: ' + error);
-//            },
-//            function () {
-//                console.log('DB init success');
-//                deferred.resolve();
-//            }
-//        );
 
     return deferred.promise();
 }
 
 function removeSuccess() {
     console.log(">SUCCESS, removed sonna.sqlite!!!");
-//        alert(">SUCCESS, removed sonna.sqlite!!!");
 }
 
 function removeError() {
     console.log(">ERROR, unable to remove sonna.sqlite!!!");
-//        alert(">ERROR, unable to remove sonna.sqlite!!!");
-
-
 }
 
-
-OpenMyDatabase = function() {
+function OpenMyDatabase(){
             //if not the debugger of my MAC
     if(typeof mac_browser_test === 'undefined'){
         //sqlitePlugin: "createFromLocation: 1" NEVER WORKS WITH ME
-        db = window.sqlitePlugin.openDatabase({name:"sonna.sqlite"});
+        database = window.sqlitePlugin.openDatabase({name:"sonna.sqlite"});
 
-    } else { //My Debugger
+    } else { //My Desktop Debugger
 
-        db = window.openDatabase("sqlite", "1.0", "sqlite", 20000000);
-        db.transaction(
+        database = window.openDatabase("sqlite", "1.0", "sqlite", 20000000);
+        database.transaction(
             function (tx) {
                 createTable(tx);
                 addSampleData(tx);
             },
             function (error) {
                 console.log('Transaction error: ' + error);
-                deferred.reject('Transaction error: ' + error);
+                //deferred.reject('Transaction error: ' + error);
             },
             function () {
                 console.log('DB init success');
                 //deferred.resolve();
             }
         );
-
-
     }
-
-
 }
 
-//    function copySuccess() {
 function copySuccess () {
     console.log(">Success in copying sonna.sqlite");
-//        alert(">Success in copying sonna.sqlite");
 
     OpenMyDatabase();
 
-//        doDisplay("g2b1", "0");
-
         var page_id = "0";
 
-        db.transaction(
+        database.transaction(
             function (tx) {
-                var sql = "SELECT * FROM pages where page_id MATCH ?";
-                tx.executeSql(sql, [page_id], function (tx, results) {
-                    var len = results.rows.length;
-                    if(len != 1) {
-                        //I expect an end of ids
-                        console.log('>results != 1 !!! , len=' + len);
-                        return;
-                    }
-
-                    page = results.rows.item(0);
-                    console.log(">Displaying initial page");
-
-//                        doDisplay(book_code, page_id);
-
-//                      $.cookie("book_code", page.book_code);
-//                      $.cookie("page_id", page.page_id);
-                    //cookies are not supported
-                    window.localStorage.setItem("book_code", page.book_code);
-                    window.localStorage.setItem("page_id", page.page_id);
-
-                    $('#article-title').empty();
-                    $('#article-title').append(page.title);
-
-                    $('article-body').empty();
-
-                    var parts = page.page.split("##");
-
-                    $('#article-body').append(parts[0]);
-                    if(parts.length > 1) {
-                        $('#article-body').append("<hr>" + parts[1]);
-                    }
-
-                    //Hide Hour glass
-                    $.mobile.loading( "hide", {
-                          text: "",
-                          textVisible: false,
-                          theme: "z",
-                          html: ""
-                        });
 
 
-//                        $('article-body').append(page.page);
-//                        $('.page_fts').empty();
-//                        $('.page_fts').append(page.page_fts);
+              //Hide Hour glass
+                $.mobile.loading( "hide", {
+                      text: "",
+                      textVisible: false,
+                      theme: "z",
+                      html: ""
+                    });
 
-//                        deferred.resolve(page);
-//                  alert("rows returned are " + results.rows.length)
-//                  console.log('rows returned: ' + results[0].book_code);
-                });
+                doDisplay("g2b1", 0);
+
+
             },
             function (error) {
-//                   deferred.reject("Display Error: " + error.message);
                console.log(">Error: " + error.message);
             }
         );
 
-//
-////        display("g2b1", "0").done(function (page) {
-//
-//            $.cookie("book_code", page.book_code);
-//            $.cookie("page_id", page.page_id);
-//
-//            $('.article-title').empty();
-//            $('.article-title').append(page.title);
-//
-//            $('.page').empty();
-//            $('.page').append(page.page);
-//            $('.page_fts').empty();
-//            $('.page_fts').append(page.page_fts);
-
-
-
-//            doTabweeb (page.title, book_code, page_id, page.parent_id);
-
-//        });
-
-
 }
-
 
 function copyError() {
     console.log(">Failed to copy sonna.sqlite!!!");
-//        alert(">Failed to copy sonna.sqlite!!!");
 }
-
 
 function createTable (tx) {
     tx.executeSql('DROP TABLE IF EXISTS pages');
@@ -216,16 +102,17 @@ function createTable (tx) {
         "page_id, parent_id, book_code, title, page, page_fts);";
     tx.executeSql(sql, null,
         function () {
-            console.log('Create table success');
+            //console.log('Create table success');
+
         },
         function (tx, error) {
             alert('Create table error: ' + error.message);
         });
     }
 
- function addSampleData (tx, employees) {
+ function addSampleData (tx) {
 
-    var employees = [
+    var ahadith = [
         {"page_id": "0", "parent_id": "NO_PARENT", "book_code": "g2b1", "title":"الأول", "page": "أَبَا سُفْيَانَ بْنَ حَرْبٍ أَخْبَرَهُ أَنَّ هِرَقْلَ أَرْسَلَ ", "page_fts": "قال" },
         {"page_id": "1", "parent_id": "0", "book_code": "g2b1", "title":"الثاني", "page": "البخاري", "page_fts": "البخاري" },
         {"page_id": "2", "parent_id": "0", "book_code": "g2b1", "title":"الثالث", "page": "حدثنا", "page_fts": "البخاري" },
@@ -235,65 +122,56 @@ function createTable (tx) {
         {"page_id": "6", "parent_id": "5", "book_code": "g2b1", "title":"Seven", "page": "الله يقول", "page_fts": "اي حاجه" },
         {"page_id": "7", "parent_id": "5", "book_code": "g2b1", "title":"Eight", "page": "انما الاعمال بالنيات", "page_fts": "اما الاعمال بالنيات" }
     ];
-    var l = employees.length;
+    var l = ahadith.length;
     var sql = "INSERT OR REPLACE INTO pages " +
         "(page_id, parent_id, book_code, title, page, page_fts) " +
         "VALUES (?, ?, ?, ?, ?, ?)";
     var e;
     for (var i = 0; i < l; i++) {
-        e = employees[i];
+        e = ahadith[i];
         tx.executeSql(sql, [e.page_id, e.parent_id, e.book_code, e.title, e.page, e.page_fts],
             function () {
-                console.log('INSERT success');
+                //console.log('INSERT success');
             },
             function (tx, error) {
-//                    alert('INSERT error: ' + error.message);
+                alert('INSERT error: ' + error.message);
                 console.log('INSERT error: ' + error.message);
-
             });
     }
+    console.log('Create sample data success');
+
 }
 
-function search(queryString) {
+function getSearchHits(queryString) {
     var deferred = $.Deferred();
-    db.transaction(
+    database.transaction(
         function (tx) {
             var sql = "SELECT * FROM pages where page_fts MATCH '" + queryString + "'";
             tx.executeSql(sql, [], function (tx, results) {
                 //Fill Hits Info
-                var len = results.rows.length,
-                    hits = [], i = 0;
-                for (; i < len; i = i + 1) {
+                var len = results.rows.length;
+                var hits = [];
+                for (var i = 0; i < len; i = i + 1) {
                     hits[i] = results.rows.item(i);
                 }
-
                 deferred.resolve(hits);
                 console.log("hits returned are " + results.rows.length)
             });
         },
         function (error) {
-            console.log('>ERROR: ' + error.message);
-            deferred.reject("Transaction Error: " + error.message);
+            console.log('>Search ERROR: ' + error.message);
+            deferred.reject("Search Error: " + error.message);
         }
     );
     return deferred.promise();
 }
 
-function display (book_code, page_id) {
+function getDisplay (book_code, page_id) {
     var deferred = $.Deferred();
 
+    //OpenMyDatabase();
 
-    //if(typeof mac_browser_test === 'undefined') {
-    //    db = window.sqlitePlugin.openDatabase({name: "sonna.sqlite"});
-    //}else {
-    //
-    //
-    //}
-
-    //Duplicate open just to check
-    OpenMyDatabase();
-
-    db.transaction(
+    database.transaction(
         function (tx) {
             var sql = "SELECT * FROM pages where page_id MATCH ?";
             tx.executeSql(sql, [page_id], function (tx, results) {
@@ -306,7 +184,6 @@ function display (book_code, page_id) {
 
                 page = results.rows.item(0);
                 deferred.resolve(page);
-//                  alert("rows returned are " + results.rows.length)
 //                  console.log('rows returned: ' + results[0].book_code);
             });
         },
@@ -320,7 +197,7 @@ function display (book_code, page_id) {
 
 function getKidsNodes(book_code, page_id) {
     var deferred = $.Deferred();
-    db.transaction(
+    database.transaction(
         function (tx) {
             var sql = "SELECT * FROM pages where parent_id MATCH ?";
             tx.executeSql(sql, [page_id], function (tx, results) {
@@ -351,7 +228,7 @@ function getKidsNodes(book_code, page_id) {
 //FIXME support multiple books by adjusting queries
 function getParentNode(book_code, page_id, parent_id) {
     var deferred = $.Deferred();
-    db.transaction(
+    database.transaction(
         function (tx) {
             var sql = "SELECT * FROM pages where page_id MATCH ?";
             var nodeId = parent_id;
@@ -377,6 +254,3 @@ function getParentNode(book_code, page_id, parent_id) {
     return deferred.promise();
 }
 
-
-
-//}
--- sonna/www/index.js
+++ sonna/www/index.js
@@ -1,93 +1,82 @@
-// We use an "Immediate Function" to initialize the application to
-// avoid leaving anything behind in the global scope
 
-//var search = new PageService();
-
-////Module pattern
-//(function () {
-
-    /* ---------------------------------- Local Variables ---------------------------------- */
-
-
-    /* --------------------------------- Event Registration -------------------------------- */
-
-
-    if(typeof mac_browser_test === 'undefined') {
-
-        document.addEventListener('deviceready', onDeviceReady, false);
-    } else {
-        initialize();
-    }
-
-
-
-  function onDeviceReady() {
-
+if (typeof mac_browser_test === 'undefined') {
+    document.addEventListener('deviceready', onDeviceReady, false);
+} else {
     initialize();
+}
 
+function onDeviceReady() {
+    console.log(">device is ready");
+    initialize();
+}
+
+function initialize() {
+
+    //console.log(">Override alert to use native interface");
+    //override the window.alert() function and replace its default implementation
+    if (navigator.notification) { // Override default HTML alert with native dialog
+        window.alert = function (message) {
+            navigator.notification.alert(
+                message,    // message
+                null,       // callback
+                "Sonna",    // title
+                'OK'        // buttonName
+            );
+        };
     }
 
+    initializeDB();
 
-    function initialize() {
+}
 
-
-          console.log(">device is ready");
-
-        console.log(">Override alert to use native interface");
-
-        //override the window.alert() function and replace its default implementation
-        if (navigator.notification) { // Override default HTML alert with native dialog
-              window.alert = function (message) {
-                  navigator.notification.alert(
-                      message,    // message
-                      null,       // callback
-                      "Sonna",    // title
-                      'OK'        // buttonName
-                  );
-              };
-          }
-
-        console.log(">initialize search database");
-        initializeDB();
-
-//      console.log(">initial doDisplay  is going to be called");
-
-    }
-
-
-
-    //FIXME MOVE to onDeviceReady
-    // SWIPE Support for touch screens
-    $( document ).on( "pagecreate", "#demo-page", function() {
-        $( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
-            // We check if there is no open panel on the page because otherwise
-            // a swipe to close the left panel would also open the right panel (and v.v.).
-            // We do this by checking the data that the framework stores on the page element (panel: open).
-            if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
-                if ( e.type === "swipeleft" ) {
-                    $( "#right-panel" ).panel( "open" );
-                } else if ( e.type === "swiperight" ) {
-                    $( "#left-panel" ).panel( "open" );
-                }
+//FIXME MOVE to onDeviceReady
+// SWIPE Support for touch screens
+$(document).on("pagecreate", "#demo-page", function () {
+    $(document).on("swipeleft swiperight", "#demo-page", function (e) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ($(".ui-page-active").jqmData("panel") !== "open") {
+            if (e.type === "swipeleft") {
+                $("#right-panel").panel("open");
+            } else if (e.type === "swiperight") {
+                $("#left-panel").panel("open");
             }
-        });
+        }
     });
-
-    /* ---------------------------------- Local Functions ---------------------------------- */
-
+});
 
 
-
-//
 //}());
 
+function getSpaces(count) {
+    var spaces = ""
+    for (var i = 0; i < count; i++) {
+        spaces += "&nbsp;&nbsp;&nbsp;";
+    }
+    return spaces;
+}
 
+//Example: var link = String.format('<a href="{0}/{1}/{2}" title="{3}">{3}</a>', url, year, titleEncoded, title);
+function strf() {
+    // The string containing the format items (e.g. "{0}")
+    // will and always has to be the first argument.
+    var theString = arguments[0];
+    // start with the second argument (i = 1)
+
+//    for(var i in allImgs)
+    for (var i = 1; i < arguments.length; i++) {
+        // "gm" = RegEx options for Global search (more than one instance)
+        // and for Multiline search
+        var regEx = new RegExp("\\{" + (i - 1) + "\\}", "gm");
+        theString = theString.replace(regEx, arguments[i]);
+    }
+    return theString;
+}
 
 function doDisplay(book_code, page_id) {
     //call display
-    display(book_code, page_id).done(function (result) {
-//        $.cookie("book_code", page.book_code);
-//        $.cookie("page_id", page.page_id);
+    getDisplay(book_code, page_id).done(function (result) {
         window.localStorage.setItem("book_code", result.book_code);
         window.localStorage.setItem("page_id", result.page_id);
 
@@ -98,164 +87,124 @@ function doDisplay(book_code, page_id) {
         var parts = result.page.split("##");
 
         $('#article-body').append(parts[0]);
-        if(parts.length > 1) {
+        if (parts.length > 1) {
             $('#article-body').append("<hr>" + parts[1]);
         }
 
 //        $('.page_fts').empty();
 //        $('.page_fts').append(page.page_fts);
 
-        doTabweeb (page.title, book_code, page_id, page.parent_id);
+        doTabweeb(page.title, book_code, page_id, page.parent_id);
 
     });
 
 }
 
 function doPrevious() {
-//    var page_id = $.cookie("page_id");
     var page_id = window.localStorage.getItem("page_id");
-
     page_id--; //automatic conversion to integer
-    if(page_id < 0) {
-        //just do nothing
+    if (page_id < 0) {
         return;
     }
-//    book_code = $.cookie('book_code');
     var book_code = window.localStorage.getItem("book_code");
-
     console.log('Previous: page_id = ' + page_id + ', book_code:' + book_code);
     page_id = page_id + ""; //convert to text as required by sqlite query
     doDisplay(book_code, page_id);
 }
 
 function doNext() {
-//    var page_id = $.cookie("page_id");
     var page_id = window.localStorage.getItem("page_id");
-
     page_id++;
-//    book_code = $.cookie('book_code');
     var book_code = window.localStorage.getItem("book_code");
-
     console.log('Next: page_id = ' + page_id + ', book_code:' + book_code);
     page_id = page_id + "";
     doDisplay(book_code, page_id);
 }
 
 function doSearch() {
-//    alert("search");
     var queryString = $('#query-string').val();
-    alert(queryString)
+    //alert(queryString)
     queryString = queryString.trim();
-    if(queryString.length <= 0) {
+    if (queryString.length <= 0) {
         return;
     }
 
-    service.search(queryString).done(function (hits) {
+//        $('#search-hits-count').text(hitsCount + " (" + total_count + ")");
+
+    getSearchHits(queryString).done(function (hits) {
+        $('#search-hits').empty();
         var len = hits.length;
-        i = 0;
-        for (; i < len; i = i + 1) {
-//            console.log(hits[i].book_code + ", " + hits[i].page + ", " + hits[i].page_fts);
-            $('.hits-list').empty();
-            for (var i = 0; i < len; i++) {
-                e = hits[i];
-                $('.hits-list').append('<li><a href="#employees/' + e.id + '">' + e.page + ' ' + e.page_fts + '</a></li>');
-            }
+        for (var i = 0; i < len; i = i + 1) {
+            var title = hits[i].title;
+            var page_id = hits[i].page_id;
+            var book_code = hits[i].book_code;
+            //var searchWords = result_obj.words.join(",");
+            var params = strf("('{0}', '{1}')", book_code, page_id);
+            var row = strf("<tr><td><a href=\"javascript:doDisplay{0}\">{1}</a></td></tr>", params, title);
+            $('#search-hits').append(row);
+
         }
 
     });
 }
 
-
-function doTabweeb (title, book_code, page_id, parent_id) {
-
-//    getParentNode(book_code, page_id, parent_id).done(function (parents) {
-//        $('#tabweeb-tree-head').empty();
-//        console.log('Getting parent of page_id=' + page_id);
-//        for(var i = parents.length-1; i >= 0;i--) {
-//            $('#tabweeb-tree-head').append(parents[i].title + "<br>");
-//        }
-//        //display title
-//        $('#tabweeb-tree-head').append("<i><b>" + page.title + "</b></i><br>");
-//    });
-
+function doTabweeb(title, book_code, page_id, parent_id) {
     $('#tabweeb-tree-head').empty();
 
-    var level = 0
-    var parents = []
+    var level = 0;
+    showParentNodePath(book_code, page_id, parent_id);
+    //decorate nodes
 
-    showParentNodePath(title, book_code, page_id, parent_id, level, parents);
+    //var treeHeadHtml = $('#tabweeb-tree-head');
+    //getSpaces(level) +
 
-        //display title
+//     for(var i = 0 ; i < pathNodes.length ; i++ ) {
+//         var anchor = strf("<a href='javascript:doDisplay(\"{0}\", \"{1}\")')>{2}</a>",
+//                 pathNodes[i].book_code, pathNodes[i].parent_id, pathNodes[i].parent_title);
+//         $('#tabweeb-tree-head').append(anchor + "<br>");
+
+//     }
+
+    //display title
     $('#tabweeb-tree-head').append("<i><b>" + title + "</b></i><br>");
 
-
-////////////////////////////////////////////////
-
+    ////////////////////////////////////////////////
 
     getKidsNodes(book_code, page_id, parent_id).done(function (kids) {
         $('#tabweeb-tree-body').empty();
 
         var len = kids.length;
-        for(var i = 0; i < len ; i++) {
+        for (var i = 0; i < len; i++) {
             var hrefParameters = "('" + book_code + "', '" + kids[i].page_id + "')";
             $('#tabweeb-tree-body').append("<a href=\"javascript:doDisplay" + hrefParameters +
-                    "\">" + kids[i].title + "</a>");
+            "\">" + kids[i].title + "</a>");
             $('#tabweeb-tree-body').append("<br>");
         }
 
     });
 }
 
-function showParentNodePath(title, book_code, page_id, parent_id, level, parents) {
+//recursive function
+function showParentNodePath(book_code, page_id, parent_id) {
 
-    level += 1;
-    if(page_id.valueOf() != "0") {
+    if (page_id.valueOf() != "0") {
 
         getParentNode(book_code, page_id, parent_id).done(function (parent) {
 
-            if( parent != undefined ) {
-                //prepend: append at the start
-//                var hrefParameters = "('" + book_code + "', '" + kids[i].page_id + "')";
-                $('#tabweeb-tree-head').prepend(getSpaces(level) + parent.title + "<br>");
-                parents.push(parent);
+            if (parent != undefined) {
+                //prepend: insert at the start
+                var anchor = strf("<a href='javascript:doDisplay(\"{0}\", \"{1}\")')>{2}</a>", book_code, parent_id, parent.title);
+                $('#tabweeb-tree-head').prepend(anchor + "<br>");
+//                 pathNodes.push({'book_code': book_code, 'parent_id': parent_id, 'parent_title': parent.title});
             }
 
             //Recursive call
-            if(parent != undefined && parent.page_id != "0") {
-                showParentNodePath(title, book_code, parent.page_id, parent.parent_id, level, parents)
+            if (parent != undefined && parent.page_id != "0") {
+                showParentNodePath(book_code, parent.page_id, parent.parent_id)
             }
 
         });
     }
-
-}
-
-function getSpaces (count) {
-    var spaces = ""
-    for(var i = 0; i < count ; i++) {
-        spaces += "&nbsp;&nbsp;&nbsp;";
-    }
-    return spaces;
 }
 
 
-//
-//http://www.javascriptkit.com/javatutors/loadjavascriptcss.shtml
-//
-//function loadjscssfile(filename, filetype){
-//    if (filetype=="js"){ //if filename is a external JavaScript file
-//        var fileref=document.createElement('script')
-//        fileref.setAttribute("type","text/javascript")
-//        fileref.setAttribute("src", filename)
-//    }
-//    else if (filetype=="css"){ //if filename is an external CSS file
-//        var fileref=document.createElement("link")
-//        fileref.setAttribute("rel", "stylesheet")
-//        fileref.setAttribute("type", "text/css")
-//        fileref.setAttribute("href", filename)
-//    }
-//    if (typeof fileref!="undefined")
-//        document.getElementsByTagName("head")[0].appendChild(fileref)
-//}
-//
-//loadjscssfile("myscript.js", "js") //dynamically load and add this .js file
